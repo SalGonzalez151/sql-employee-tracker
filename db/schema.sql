71304b445c13f3967422693137355a9c8526fc24
Sal Gonzalez <imdone151@gmail.com>
2023-11-07 12:25:51 -0800

    added sql tables

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,25 @@
 CREATE DATABASE employer_tracker_db;
 
 USE employer_tracker_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL NOT NULL,
+    department_id int,
+    FOREIGN KEY deparment_id REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY role_id REFERENCES role(id)
+);
